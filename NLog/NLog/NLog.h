8e290702bbb04006ad607524d9715f3abd2783cb
Nghia Nguyen <nghia@misfit.com>
2016-02-12 09:12:45 +0700

    init project

---
+++ NLog/NLog/NLog.h
@@ -0,0 +1,19 @@
+//
+//  NLog.h
+//  NLog
+//
+//  Created by Nghia Nguyen on 2/12/16.
+//  Copyright © 2016 knacker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NLog.
+FOUNDATION_EXPORT double NLogVersionNumber;
+
+//! Project version string for NLog.
+FOUNDATION_EXPORT const unsigned char NLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NLog/PublicHeader.h>
+
+
